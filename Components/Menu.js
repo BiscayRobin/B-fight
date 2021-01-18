a6c3d914216c3da76b779beb4cd26a50edb5ca75
E-stein <eloise.stein@outlook.fr>
2021-01-18 17:12:46 +0100

    Ajout du css

--- Components/Menu.js
+++ Components/Menu.js
@@ -1,15 +1,34 @@
 import React from 'react'
-import { View, Button, Text } from 'react-native'
+import { StyleSheet, View, Button, Text } from 'react-native'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 class Menu extends React.Component {
   render() {
     return (
-      <View>
-        <Button title="Mode solo" onPress={() => {}}/>
-        <Button title="Mode multijoueur" onPress={() => {}}/>
+      <View style={styles.view}>
+        <Text style={styles.title}> B-FIGHT </Text>
+        <Button type="solid" raised="true" color='#034f84' title="Single Player" onPress={() => {}}/>
+        <Button type="solid" raised="true" color='#034f84' title="Multi Player" onPress={() => {}}/>
+        <Button type="solid" raised="true" color='#034f84' title="Options" onPress={() => {}}/>
+        <Button type="solid" raised="true" color='#034f84' title="About us" onPress={() => {}}/>
+        <Text style={styles.version}> Version 1.0 </Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  view: {
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: '#92a8d1'
+  },
+  title: {
+    textAlign: "center"
+  },
+  version: {
+    textAlign: "center"
+  }
+})
 
 export default Menu
