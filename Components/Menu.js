47293d4df8b4f9810356efc9dbe33b29adbce27a
E-stein <eloise.stein@outlook.fr>
2021-01-18 18:04:13 +0100

    modification des styles des boutons

--- Components/Menu.js
+++ Components/Menu.js
@@ -2,33 +2,65 @@ import React from 'react'
 import { StyleSheet, View, Button, Text } from 'react-native'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
 class Menu extends React.Component {
   render() {
     return (
-      <View style={styles.view}>
-        <Text style={styles.title}> B-FIGHT </Text>
-        <Button type="solid" raised="true" color='#034f84' title="Single Player" onPress={() => {}}/>
-        <Button type="solid" raised="true" color='#034f84' title="Multi Player" onPress={() => {}}/>
-        <Button type="solid" raised="true" color='#034f84' title="Options" onPress={() => {}}/>
-        <Button type="solid" raised="true" color='#034f84' title="About us" onPress={() => {}}/>
-        <Text style={styles.version}> Version 1.0 </Text>
+      <View style={styles.container}>
+        <View style={styles.titleWrapper}>
+          <Text style={styles.title}> Welcome to B-fight ! </Text>
+        </View>
+        <View style={styles.buttonWrapper}>
+          <Button type="solid" raised="true" color='#034f84' title="Single Player" onPress={() => {}} />
+          <Separator />
+          <Button type="solid" raised="true" color='#034f84' title="Multi Player" onPress={() => {}} />
+          <Separator />
+          <Button type="solid" raised="true" color='#034f84' title="Options" onPress={() => {}}/>
+          <Separator />
+          <Button type="solid" raised="true" color='#034f84' title="About us" onPress={() => {}}/>
+        </View>
+        <View style={styles.textWrapper}>
+          <Text style={styles.version}> Version 1.0 </Text>
+        </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  view: {
-    marginTop: 10,
-    marginBottom: 10,
+  container: {
+    flex: 1,
     backgroundColor: '#92a8d1'
   },
+  titleWrapper: {
+    height: hp('10%'), // 5% of height device screen
+    width: wp('100%')   // 100% of width device screen
+  },
+  buttonWrapper: {
+    height: hp('30%'), // 5% of height device screen
+    width: wp('100%'),   // 100% of width device screen
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textWrapper: {
+    height: hp('10%'), // 5% of height device screen
+    width: wp('100%')   // 100% of width device screen
+  },
   title: {
-    textAlign: "center"
+    textAlign: "center",
+    fontSize: hp('5%'),
+    color: '#f7786b'
   },
   version: {
     textAlign: "center"
-  }
+  },
+  separator: {
+    height: hp('5%'),
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
 })
 
 export default Menu
