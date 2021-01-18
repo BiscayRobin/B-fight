d7eb6a970fca1141f364dce3ec9c93d065d56b9f
E-stein <eloise.stein@outlook.fr>
2021-01-18 16:11:59 +0100

    Création Menu

---
+++ Components/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Button, Text } from 'react-native'
+
+class Menu extends React.Component {
+  render() {
+    return (
+      <View>
+        <Button title="Mode solo" onPress={() => {}}/>
+        <Button title="Mode multijoueur" onPress={() => {}}/>
+      </View>
+    )
+  }
+}
+
+export default Menu
