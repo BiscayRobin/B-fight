ca884915fb6b3fb3c312f78627495dda1e974104
Arthur <rauch.arthur@gmail.com>
2021-01-31 22:56:01 +0100

    storage package

--- Components/HighScore.js
+++ Components/HighScore.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { StyleSheet, View, Button, Text } from 'react-native'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
+
 
 class HighScore extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  componentDidMount(){
+    
+  }
   render() {
     return (
       <View>
