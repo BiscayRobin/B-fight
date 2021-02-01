4b18a19a420dd9c2ce8c093eb4c0da91821b3d78
E-stein <eloise.stein@outlook.fr>
2021-02-01 22:32:11 +0100

    about us page

--- Components/AboutUs.js
+++ Components/AboutUs.js
@@ -5,11 +5,76 @@ import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-nativ
 class AboutUs extends React.Component {
   render() {
     return (
-      <View>
-        <Text> About us </Text>
+      <View style={styles.container}>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}> Who are we ? </Text>
+      </View>
+      <View style={styles.textWrapper}>
+        <Text style={styles.text}> We are a community of developers and we created this application for free.  </Text>
+      </View>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}> Why ? </Text>
+      </View>
+      <View style={styles.textWrapper}>
+        <Text style={styles.text}> We developed this game because
+        we love programming and we want to make it fun for everyone to have fun
+        while getting smarter. </Text>
+      </View>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}> Any suggestions ? </Text>
+      </View>
+      <View style={styles.textWrapper}>
+        <Text style={styles.text}> If you have any suggestions to help us improve our application,
+        you can contact us at this email address: suggestions@bfight.com </Text>
+      </View>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}> Do you want to thank us? </Text>
+      </View>
+      <View style={styles.textWrapper}>
+        <Text style={styles.text}> If you want to thank us or give us more ways to improve our application,
+        you can make a donation here: https://www.buymeacoffee.com/b-fight</Text>
+      </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#92a8d1'
+  },
+  titleWrapper: {
+    height: hp('10%'), // 5% of height device screen
+    width: wp('100%')   // 100% of width device screen
+  },
+  buttonWrapper: {
+    height: hp('30%'), // 5% of height device screen
+    width: wp('100%'),   // 100% of width device screen
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textWrapper: {
+    height: hp('10%'), // 5% of height device screen
+    width: wp('100%')   // 100% of width device screen
+  },
+  title: {
+    textAlign: "center",
+    fontSize: hp('5%'),
+    color: '#f7786b'
+  },
+  version: {
+    textAlign: "center"
+  },
+  separator: {
+    height: hp('5%'),
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: hp('2%'),
+    color: '#c94c4c'
+  }
+})
 
 export default AboutUs
